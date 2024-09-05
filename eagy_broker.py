d1846dfbbae54f1f0e6c87ab7f399b14b322a3a6
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-05 22:40:47 +0200

    add probabilities

--- eagy_broker.py
+++ eagy_broker.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class EagyBroker:
     def __init__(self):
         self.initial_prices = {}
@@ -10,27 +12,27 @@ class EagyBroker:
         share = stock_exchanger.share
         current_price = stock_exchanger.current_price
         self.initial_prices[share], self.current_prices[share] = current_price, current_price
-        self.num_shares_sold[share] = 0
-        self.num_shares_bought[share] = 0
+        self.num_shares_sold[share], self.num_shares_bought[share] = 0, 0
 
     def process_event(self, event):
         """Processes a queue event (price or volume)."""
         share, event_type, *data = event
         if event_type == 'price':
             self.current_prices[share] = data[0]
+            print(self.suggest(share))
+            self.num_shares_sold[share], self.num_shares_bought[share] = 0, 0
         elif event_type == 'volume':
-            self.num_shares_sold[share] = data[0]
-            self.num_shares_bought[share] = data[1]
-        print(self.suggest(share))
+            self.num_shares_sold[share] += data[0]
+            self.num_shares_bought[share] += data[1]
 
     def suggest(self, share):
         """Suggests an operation."""
-        initial_price, current_price, num_shares_sold, num_shares_bought = self.initial_prices[
-            share], self.current_prices[share], self.num_shares_sold[share], self.num_shares_bought[share]
+        initial_price, current_price = self.initial_prices[share], self.current_prices[share]
+        num_shares_sold, num_shares_bought = self.num_shares_sold[share], self.num_shares_bought[share]
         print(
             f"\nnum_shares_sold: {num_shares_sold} num_shares_bought: {num_shares_bought} initial_price: {initial_price} current_price: {current_price}")
         if num_shares_sold > 2000 and current_price <= initial_price * 0.9:
-            return f"Eagy Broker suggests to BUY {share}"
+            return f"{datetime.now()} - Eagy Broker suggests to BUY {share}"
         elif num_shares_bought > 2000 and current_price >= initial_price * 1.1:
-            return f"Eagy Broker suggests to SELL {share}"
-        return f"Eagy Broker suggests to HOLD {share}"
+            return f"{datetime.now()} - Eagy Broker suggests to SELL {share}"
+        return f"{datetime.now()} - Eagy Broker suggests to HOLD {share}"
--- stock_exchanger.py
+++ stock_exchanger.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, random
 import threading
 import time
 from queue import Queue
@@ -18,16 +18,29 @@ class StockExchanger:
         """Broadcasts the current price of the share every minute"""
         while True:
             with self.lock:
-                self.current_price = randint(60, 140)
+                self.update_price()
                 self.queue.put((self.share, 'price', self.current_price))
+                self.num_shares_sold, self.num_shares_bought = 0, 0
             time.sleep(60)
 
     def broadcast_volume(self):
         """Broadcasts the number of shares sold or bought every second"""
         while True:
             with self.lock:
-                self.num_shares_sold = randint(1800, 2200)
-                self.num_shares_bought = randint(1800, 2200)
-                self.queue.put(
-                    (self.share, 'volume', self.num_shares_sold, self.num_shares_bought))
+                if random() < 0.5:
+                    shares_sold, shares_bought = randint(1, 1000), 0
+                    self.num_shares_sold += shares_sold
+                else:
+                    shares_bought, shares_sold  = randint(1, 1000), 0
+                    self.num_shares_bought += shares_bought
+                self.queue.put((self.share, 'volume', shares_sold, shares_bought))
             time.sleep(1)
+
+    def update_price(self):
+        """Updates the price based on the amount of shares bought or sold."""
+        if self.num_shares_bought + self.num_shares_sold == 0: return
+        p_increase = self.num_shares_sold / (self.num_shares_bought + self.num_shares_sold)
+        if random() < p_increase:
+            self.current_price = int(self.current_price * 1.1)
+        else:
+            self.current_price = int(self.current_price * 0.9)
