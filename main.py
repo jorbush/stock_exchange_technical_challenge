49047881a76890b88fd51490da6d163f653244a6
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-05 08:45:54 +0200

    simulate stock exchanges

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from random import randint
+
 def stock_exchanger_broadcast():
-    current_share_price = 0
-    num_shares_sold = 0
-    num_shares_bought = 0
-    return { current_share_price, num_shares_sold, num_shares_bought }
+    current_share_price = randint(60, 140)
+    num_shares_sold = randint(1800, 2200)
+    num_shares_bought = randint(1800, 2200)
+    return current_share_price, num_shares_sold, num_shares_bought
 
 def eagy_broker_suggestion(share, num_shares_sold, num_shares_bought, initial_share_price, current_share_price):
-    overall_number = num_shares_sold + num_shares_bought
-    if num_shares_sold > 2000 and current_share_price <= (initial_share_price - (initial_share_price * (10/100))):
-        # Eagy Broker suggests to buy
-        raise NotImplementedError
-    elif  num_shares_bought > 2000 and current_share_price >= (initial_share_price + (initial_share_price * (10/100))):
-        # Eagy Broker suggests to sell
-        raise NotImplementedError
+    print(f"num_shares_sold: {num_shares_sold} num_shares_bought: {num_shares_bought} initial_share_price: {initial_share_price} current_share_price: {current_share_price}")
+    if num_shares_sold > 2000 and current_share_price <= (initial_share_price * 0.9):
+        return f"Eagy Broker suggests to BUY {share}"
+    elif  num_shares_bought > 2000 and current_share_price >= (initial_share_price * 1.1):
+        return f"Eagy Broker suggests to SELL {share}"
+    return f"Eagy Broker suggests to HOLD {share}"
 
 if __name__ == '__main__':
-    raise NotImplementedError
+    share = "AMZN"
+    initial_share_price = 100
+    print(f"Initial share price for {share}: {initial_share_price}")
+    # simulate 10 stock exchanges
+    for i in range(10):
+        current_share_price, num_shares_sold, num_shares_bought = stock_exchanger_broadcast()
+        suggestion = eagy_broker_suggestion(share, num_shares_sold, num_shares_bought, initial_share_price, current_share_price)
+        print(suggestion)
